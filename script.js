4992fc9c2427787e74f90b881c75f19553a40dd2
divyasreenivas <n170860@rguktn.ac.in>
2023-08-10 20:22:36 +0530

    created the manifest.json and render

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-let myleads =["www","divya","sreee"]
+let myleads =[]
 const inputEl = document.getElementById("input-el")
 const inputbtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 console.log(ulEl)
+console.log(localStorage.getItem("myleads"))
+console.log(myleads)
 
 inputbtn.addEventListener("click",function(){
     myleads.push(inputEl.value)
-    console.log(myleads)
+    inputEl.value=""
+    localStorage.setItem("myleads",JSON.stringify(myleads))
+    rendercode()
+    console.log(localStorage.getItem("myleads"))
 })
 
+function rendercode(){
+let listItems =""
 for (let i =0 ;i<myleads.length;i++){
-    ulEl.innerHTML+= "<li>"+myleads[i]+ " " +"</.li>"
+    listItems += `
+    <li>
+       <a target ='_blank' href='${myleads[i]}'> 
+       ${ myleads[i]} 
+      </a>
+     </li>`
+    
+    console.log(listItems)
+}
+ulEl.innerHTML=listItems
+
 }
